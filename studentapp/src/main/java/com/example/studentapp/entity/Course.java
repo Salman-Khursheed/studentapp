3dba1fa95356f0a826fcd285f4b4cf7564616695
Salman-Khursheed <iamsalman544@gmail.com>
2026-05-02 14:14:25 +0530

    Update Course.java
    Fixed the bug that was causing a error of "no course" while updating the changes in the course entry.

--- studentapp/src/main/java/com/example/studentapp/entity/Course.java
+++ studentapp/src/main/java/com/example/studentapp/entity/Course.java
@@ -17,6 +17,7 @@ public class Course {
     private List<Student> students;
 
     public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; } 
 
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
